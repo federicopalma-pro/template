ddc297183809752707407869ac207ec13aac42a6
Chicco <frcpalma@gmail.com>
2024-03-11 00:02:31 +0100

    main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from components import sum, subtract, multiply, divide
 
-a = 10
-b = 15
+
+def main():
+    a = 10
+    b = 15
+
+    print(sum(a, b))
+    print(subtract(a, b))
+    print(multiply(a, b))
+    print(divide(a, b))
 
 
-print(sum(a, b))
-print(subtract(a, b))
-print(multiply(a, b))
-print(divide(a, b))
+if __name__ == "__main__":
+    main()
