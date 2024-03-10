400ba9fd151348b3da7663c3e165a9723610fa10
Chicco <frcpalma@gmail.com>
2024-03-10 23:44:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Python Project Template
+This is a template for starting a new Python project with best practices set up from the beginning.
---
+++ main.py
@@ -0,0 +1,10 @@
+from components import sum, subtract, multiply, divide
+
+a = 10
+b = 15
+
+
+print(sum(a, b))
+print(subtract(a, b))
+print(multiply(a, b))
+print(divide(a, b))
